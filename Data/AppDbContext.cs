e6a88269a0565bb0b78a0bff597e85c276e70300
Ornat <danornat.47@gmail.com>
2023-05-03 01:56:24 +0300

    create database context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test.Models;
+
+namespace Task.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+    }
+}
