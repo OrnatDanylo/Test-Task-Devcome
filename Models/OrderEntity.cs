7a6e3ffb32ae98ab858bae4d96fa8b24011d3ad4
Ornat <danornat.47@gmail.com>
2023-05-03 01:55:23 +0300

    create oerder and user models

---
+++ Models/OrderEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test.Models
+{
+    public class OrderEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderID { get; set; }
+        public int UserId { get; set; }
+        public DateTime? OrderDate { get; set; }
+        [Column(TypeName = "money")]
+        public decimal? OrderCost { get; set; }
+        public string? ItemsDecription { get; set; }
+        public string? ShipingAddress { get; set; }
+
+
+
+        [ForeignKey("UserId")]
+        public virtual UserEntity User { get; set; }
+    }
+}
---
+++ Models/UserEntity.cs
@@ -0,0 +1,33 @@
+﻿using ServiceStack.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test.Models
+{
+    public class UserEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        [Unique]
+        public string Login { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string? Password { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string? FirstName { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string? LastName { get; set; }
+        
+        [Column(TypeName = "DATE")]
+        public DateTime? DateOfBirth { get; set; }
+
+        public char? Gender { get; set; }
+
+        public virtual ICollection<OrderEntity> Orders { get; set; }
+    }
+}
